4963b4fbf24fb27b5f06f99f86f9f240e97ee90c
Kudrat Kurban <kurtkurban@gmail.com>
2017-08-08 10:20:33 -0400

    config for the test run

--- jQuryDemo/src/test/java/com/jQuryDemo/CrossBrowserTest.java
+++ jQuryDemo/src/test/java/com/jQuryDemo/CrossBrowserTest.java
@@ -27,6 +27,7 @@ public class CrossBrowserTest {
 			driver = new ChromeDriver();
 		}
 		
+		
 		//driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
